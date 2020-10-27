65f57d330ff55c18e01b1f1f272979ad16b32cd9
RakeshP1997 <30340151+RakeshP1997@users.noreply.github.com>
2020-10-27 19:26:18 +0530

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        src: 'src/user-avatar.js',
+        dest: 'build/user-avatar.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
---
+++ README.md
@@ -0,0 +1,2 @@
+# user-avatar
+ JavaScript to make user avatars for profile pictures.
